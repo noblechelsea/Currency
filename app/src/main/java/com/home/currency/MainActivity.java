401b5093004062451d544ae19cb84dc92529f2a0
Chelsea <chelsea.yang@taiwantaxi.com.tw>
2019-12-05 10:59:47 +0800

    換算美金匯率

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -1,13 +1,40 @@
 package com.home.currency;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etNtd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNtd = findViewById(R.id.tv_ntd);
+    }
+
+    public void currency(View view) {
+        String strNtd = etNtd.getText().toString();
+        if (TextUtils.isEmpty(strNtd)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Problem")
+                    .setMessage("Please enter you NTD amount")
+                    .setPositiveButton("OK", null)
+                    .show();
+        } else {
+            float fNtd = Float.parseFloat(strNtd);
+            float fCurrency = (float) (fNtd / 30.9);
+            new AlertDialog.Builder(this)
+                    .setTitle("Result")
+                    .setMessage("USD is " + fCurrency)
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
     }
 }
